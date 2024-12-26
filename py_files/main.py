0ab9c7470feb4e041536f666a0a8029e2c6f8c68
danielsagher <daniel.sagher1@gmail.com>
2024-12-25 20:46:44 -0500

    Remade main() to be modular with variable customer sizes, and started I/O modularizing

--- py_files/main.py
+++ py_files/main.py
@@ -6,83 +6,81 @@ import datetime
 import pandas as pd
 
 
-os.chdir(os.path.join(os.getcwd(), "input_files"))
+def io(input_path):
 
-for i in os.listdir():
+    orig_path = os.path.join(input_path, "input_files")
 
-    if i.lower().strip().replace(" ", "_").startswith("invoice_data"):
-        invoice_data = pd.read_excel(
-            f"{os.path.join(os.getcwd(), i)}",
-            sheet_name=f"{i.rstrip('.xlsx')}",
-        )
-    elif i.lower().strip().replace(" ", "_").startswith("qbo"):
-        qbo = pd.read_excel(f"{os.path.join(os.getcwd(), i)}")
+    # Get list of files in input_files
+    for i in os.listdir(orig_path):
+        print(i)
+        # Load invoice_data
+        if i.lower().strip().replace(" ", "_").startswith("invoice_data"):
+
+            cur_path = os.path.join(orig_path, i)
+            invoice_data = pd.read_excel(
+                cur_path,
+                sheet_name=f"{i.rstrip('.xlsx')}",
+            )
+        # Load qbo
+        elif i.lower().strip().replace(" ", "_").startswith("qbo"):
+
+            cur_path = os.path.join(orig_path, i)
+            qbo = pd.read_excel(cur_path)
+
+    # # Tunnel into path/input_files/customers
+    # os.chdir(os.path.join(os.getcwd(), "customers"))
+
+    # # Create customer_dct from all files in customer folder
+    # customer_dct = {}
+    # for i in os.listdir():
+    #     if i.endswith(".xlsx"):
+    #         customer_dct[i] = pd.read_excel(f"{os.path.join(os.getcwd(),i)}")
+
+    # customer_dct = {
+    #     key.removesuffix(".xlsx"): value for key, value in customer_dct.items()
+    # }
 
 
-os.chdir(os.path.join(os.getcwd(), "customers"))
-
-customer_lst = []
-
-print(os.listdir())
-
-# print(customer_lst)
-customer_dct = {}
-
-for i in os.listdir():
-    if i.endswith(".xlsx"):
-        customer_dct[i] = pd.read_excel(f"{os.path.join(os.getcwd(),i)}")
-
-
-customer_dct = {
-    key.removesuffix(".xlsx").lower().replace(" ", "_"): value
-    for key, value in customer_dct.items()
-}
-
-print(customer_dct)
+io(input("File Path: "))
 
 
 def main(invoice_data, qbo, customer_dct):
 
+    # Compare FedEx invoice to QBO
     qbo_found, qbo_not_found = compare_qbo(qbo, invoice_data)
 
+    # Create Pattern Column
     qbo_not_found["Pattern"] = qbo_not_found["Reference"].apply(reg_tokenizer)
 
+    invoice_data_receiver_info = create_invoice_data_receiver_info(qbo_not_found)
+
+    # Find Reference and Receiver matches in Extensiv
+    reference_matches = list()
+    receiver_matches = list()
+
+    # Loop through Extensiv tables
     for customer, dataframe in customer_dct.items():
 
-        reference_columns = find_extensiv_reference_columns(dataframe, qbo_not_found)
-    return reference_columns
+        reference_columns = find_extensiv_reference_columns(
+            dataframe, qbo_not_found, customer
+        )
+
+        reference_matches.extend(find_value_match(dataframe, reference_columns))
+
+        extensiv_receiver_info = create_extensiv_receiver_info(dataframe)
+        print(extensiv_receiver_info)
+        receiver_matches.extend(
+            compare_receiver_info(invoice_data_receiver_info, extensiv_receiver_info)
+        )
+
+        final_df = make_final_df(reference_matches, receiver_matches, qbo_not_found)
+
+    del final_df["Pattern"]
+
+    return final_df
 
 
-# amt_reference_columns = find_extensiv_reference_columns(amt, qbo_not_found)
-# whill_reference_columns = find_extensiv_reference_columns(whill, qbo_not_found)
+if __name__ == "__main__":
 
-# gp_reference_matches = find_value_match(gp_acoustics, gp_reference_columns)
-# amt_reference_matches = find_value_match(amt, amt_reference_columns)
-# whill_reference_matches = find_value_match(whill, gp_reference_columns)
-
-# gp_receiver_info = create_extensiv_receiver_info(gp_acoustics)
-# amt_receiver_info = create_extensiv_receiver_info(amt)
-# whill_receiver_info = create_extensiv_receiver_info(whill)
-# invoice_data_receiver_info = create_invoice_data_receiver_info(qbo_not_found)
-
-# gp_receiver_matches = compare_receiver_info(
-#     invoice_data_receiver_info, gp_receiver_info
-# )
-# amt_receiver_matches = compare_receiver_info(
-#     invoice_data_receiver_info, amt_receiver_info
-# )
-# whill_receiver_matches = compare_receiver_info(
-#     invoice_data_receiver_info, whill_receiver_info
-# # )
-
-# final_df = make_final_df(gp_reference_matches, gp_receiver_matches, qbo_not_found)
-# final_df = make_final_df(amt_reference_matches, amt_receiver_matches, qbo_not_found)
-# final_df = make_final_df(
-#     whill_reference_matches, whill_receiver_matches, qbo_not_found
-# )
-
-# del final_df["Pattern"]
-# return final_df
-
-
-print(main(invoice_data, qbo, customer_dct))
+    # main(invoice_data, qbo, customer_dct)
+    pass
