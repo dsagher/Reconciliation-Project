aa1e42b90ff8567c794890db352b0b7b44b659cb
danielsagher <daniel.sagher1@gmail.com>
2025-01-15 10:16:58 -0500

    Converted much of the logic to OOP and added user stat ouput.

--- py_files/main.py
+++ py_files/main.py
@@ -5,7 +5,6 @@ from file_io import get_input, output
 import os
 import pandas as pd
 from tqdm import tqdm
-from pprint import pprint
 
 def main(invoice_data: pd.DataFrame, qbo: pd.DataFrame, customer_dct: dict ) -> pd.DataFrame:  # fmt: skip
 
@@ -32,67 +31,36 @@ def main(invoice_data: pd.DataFrame, qbo: pd.DataFrame, customer_dct: dict ) -> 
         qbo_pattern_match.reg_tokenizer
     )
 
-    # Extract receiver info from qbo_not_found into dicts
-    invoice_data_receiver_info = qbo_pattern_match.create_invoice_data_receiver_info(
-        qbo_not_found
-    )
-
     print("Searching through Extensiv tables for reference and receiver info matches")
+
     reference_matches = list()
     receiver_matches = list()
-    message = dict()
 
     # Loop through Extensiv tables and find matches
     for customer, dataframe in tqdm(customer_dct.items(), smoothing=0.1):
 
-        customer_pattern_match = pm.PatternMatch(customer)
-        # Outputs dict of dicts
-        reference_columns = customer_pattern_match.find_extensiv_reference_columns(
-            dataframe, qbo_not_found, customer
-        )
+        customer_pattern_match = pm.PatternMatch(name=customer)
+
         # find_value_match() outputs list of dicts of matches in Extensiv Table
         reference_matches.extend(
-            customer_pattern_match.find_value_match(dataframe, reference_columns)
-        )
-
-        # Extensiv receiver info in dict
-        extensiv_receiver_info = customer_pattern_match.create_extensiv_receiver_info(
-            dataframe
+            customer_pattern_match.find_value_match(
+                extensiv_table=dataframe, invoice_data=qbo_not_found
+            )
         )
 
         # Adds matches of receiver info list
         receiver_matches.extend(
             customer_pattern_match.compare_receiver_info(
-                invoice_data_receiver_info, extensiv_receiver_info
+                extensiv_table=dataframe, invoice_data=qbo_not_found
             )
         )
 
-        message[customer] = {
-            "Customer": pm.Dataset_customer.get_name(),
-            "Reference Match Count": pm.Dataset_customer.get_reference_match_count(),
-            "Receiver Match Count": pm.Dataset_customer.get_receiver_match_count(),
-            "Total Match Count": pm.Dataset_customer.get_customer_match_count(),
-            "Reference Matches": pm.Dataset_customer.get_reference_matches(),
-            "Receiver Matches": pm.Dataset_customer.get_receiver_matches(),
-        }
+        print(customer_pattern_match)
 
     # Replaces Customer PO # with Customer Name if match is found
-    final_df = pm.make_final_df(reference_matches, receiver_matches, qbo_not_found)
-
-    # Drop Pattern column from final DataFrame
-    final_df = final_df.drop(columns=["Pattern"])
-
-    print("Matching Completed")
-    print("Values found in QBO: ", pm.Dataset_found.row_num)
-    print("Values not found in QBO: ", pm.Dataset_not_found.row_num)
-
-    for customer in message:
-        pprint(f"Customer Name: {message[customer]['Customer']}")
-        pprint(f"Reference Match Count: {message[customer]['Reference Match Count']}")
-        pprint(f"Receiver Match Count: {message[customer]['Receiver Match Count']}")
-        pprint(f"Total Match Count: {message[customer]['Total Match Count']}")
-        pprint(f"Reference Matches: {message[customer]['Reference Matches']}")
-        pprint(f"Receiver Matches: {message[customer]['Receiver Matches']}")
+    final_df = customer_pattern_match.make_final_df(
+        reference_matches, receiver_matches, qbo_not_found
+    )
 
     return final_df, qbo_found
 
@@ -103,6 +71,6 @@ if __name__ == "__main__":
         path=input("File Path (or press Enter for current directory): ") or os.getcwd()
     )
     final_df, qbo_found = main(invoice_data, qbo, customer_dct)
-    # output(final_df, qbo_found)
+    output(final_df, qbo_found)
     print("All done")
     # pass
--- py_files/pattern_match.py
+++ py_files/pattern_match.py
@@ -1,23 +1,5 @@
 import pandas as pd
 import re as re
-from dataclasses import dataclass
-
-
-@dataclass
-class ReferenceMatch:
-    Reference: str
-    Name: str
-    Column: str
-    Customer: str
-    Tracking: str = None
-
-
-@dataclass
-class ReceiverMatch:
-    Address: str = None
-    Name: str = None
-    Company: str = None
-    Customer: str = None
 
 
 class PatternMatch:
@@ -29,6 +11,51 @@ class PatternMatch:
         self.reference_matches: list = []
         self.found_references: set = ()
         self.not_found_references: set = ()
+        self.extensiv_receiver_dct: dict = {}
+        self.invoice_data_receiver_dct: dict = {}
+        self.reference_counter: int = 0
+        self.receiver_counter: int = 0
+
+    def count_reference(self):
+        self.reference_counter += 1
+
+    def get_reference_count(self):
+        return self.reference_counter
+
+    def count_receiver(self):
+        self.receiver_counter += 1
+
+    def get_receiver_count(self):
+        return self.receiver_counter
+
+    def append_match(self, reference_match=None, receiver_match=None):
+
+        if receiver_match is not None or reference_match is not None:
+
+            if receiver_match:
+                self.receiver_matches.append(receiver_match)
+            elif reference_match:
+                self.reference_matches.append(reference_match)
+        else:
+            raise ValueError("Receiver Match or Reference Match Cannot Be None")
+
+    def __str__(self):
+
+        return (f"\n"
+                f"Customer: {self.name}\n"
+                f"{'-' * 70 }\n"
+                f"\n"
+                f"Total # of Reference Matches in Extensiv: {self.reference_counter}\n"
+                f"Unique # of Reference Matches in Extensiv: {len(self.reference_matches)}\n"
+                f"Reference Matches in Extensiv: {', \n'.join(map(str,self.reference_matches))}\n"
+                f"{'-' * 70 }\n"
+                f"\n"
+                f"Total # of Receiver Matches in Extensiv: {self.receiver_counter}\n"
+                f"Unique # of Receiver Matches in Extensiv: {len(self.receiver_matches)}\n"
+                f"Receiver Matches in Extensiv: {', \n'.join(map(str,self.receiver_matches))}\n"
+                f"\n"
+                f"{'-' * 70 }\n"
+        )
 
     def compare_qbo(
         self, qbo: pd.DataFrame, invoice_data: pd.DataFrame
@@ -42,6 +69,7 @@ class PatternMatch:
 
         # ? is 'Display_Name' the only key to compare against?
         # Merge qbo and invoice_data via Customer PO # and Display_Name via inner merge
+
         qbo_found = pd.merge(
             qbo,
             invoice_data,
@@ -56,18 +84,15 @@ class PatternMatch:
         qbo_found = qbo_found[invoice_cols]
 
         # Create sets of found and not found references
-        found_references = set(qbo_found["Customer PO #"].unique())
+        self.found_references_all = list(qbo_found["Customer PO #"])
+        self.found_references_unique = set(qbo_found["Customer PO #"].unique())
 
-        all_references = set(invoice_data["Customer PO #"])
-        unmatched_references = all_references - found_references
-
-        # Add to class instance
-        self.not_found_references = unmatched_references
-        self.found_references = found_references
+        self.all_references = set(invoice_data["Customer PO #"])
+        self.unmatched_references = self.all_references - self.found_references_unique
 
         # Create new DataFrame, add Customer PO #'s, and merge with invoice_data on left merge
         qbo_not_found = pd.DataFrame(
-            list(unmatched_references), columns=["Customer PO #"]
+            list(self.unmatched_references), columns=["Customer PO #"]
         )
 
         # Merge with invoice data
@@ -105,6 +130,12 @@ class PatternMatch:
         """
         col_lst = set()
 
+        if not isinstance(extensiv_table, pd.DataFrame):
+            raise ValueError("Extensiv Table Must be Pandas DataFrame")
+
+        if ref_pattern == None:
+            raise ValueError("Reference Cannot be None")
+
         # Iterate through each column in Extensiv table
         for col in extensiv_table.columns:
 
@@ -122,8 +153,7 @@ class PatternMatch:
             return col_lst
 
     def find_extensiv_reference_columns(
-        self, extensiv_table: pd.DataFrame, qbo_not_found: pd.DataFrame, customer: str
-    ) -> dict[dict]:
+        self, extensiv_table: pd.DataFrame, qbo_not_found: pd.DataFrame) -> dict[dict]:  # fmt: skip
         """
         Function: Finds all of the columns in the Extensiv table that match each 'Reference' in FedEx Invoice not in QBO
         Input: Extensiv DataFrame, FedEx Invoice DataFrame w/ added 'Pattern' column
@@ -135,83 +165,64 @@ class PatternMatch:
         """
 
         match_dct = dict()
-        suffix = 0
 
         # Iterate through Reference column in qbo_not_found
         for i, v in enumerate(qbo_not_found["Reference"]):
-
-            # If previous reference is the same as current reference, add a suffix ex: "-s1"
-            if i != 0 and v == qbo_not_found["Reference"][i - 1]:
-
-                suffix += 1
-                v = f"{v}-s{suffix}"
-
-            # Reset suffix
-            elif i != 0 and v != qbo_not_found["Reference"][i - 1]:
-                suffix = 0
 
             # Call column matcher function on each value in reference column
             match_lst = self.find_matching_columns(
                 extensiv_table, qbo_not_found["Pattern"][i]
             )
 
-            #! Maybe could just do if v not in match_dct instead of doing suffix maker
-            #! Tracking number may be unnecessary
-            #! Customer may be unnecessary too, but keeping it just incase I want to change further functions to it
             # Add match list to dictionary of dictionaries along with Tracking # and Customer
             if match_lst is not None and not pd.isna(v):
 
-                match_dct[v] = {
-                    "match_lst": match_lst,
-                    "Tracking #": (qbo_not_found["Tracking #"][i]),
-                    "Customer": customer,
-                }
+                match_dct[str(v)] = {"match_lst": match_lst}
 
         return match_dct
 
     ### - Find Extensiv Reference Columns
 
-    def find_value_match(self, extensiv_table: pd.DataFrame, reference_columns: dict) -> list[dict]:  # fmt: skip
+    def find_value_match(
+        self, extensiv_table: pd.DataFrame, invoice_data: pd.DataFrame
+    ) -> list[dict]:
+
+        reference_columns = self.find_extensiv_reference_columns(
+            extensiv_table, invoice_data
+        )
 
         match_lst = list()
 
         # Iterate through dict. First layer is references
         for reference in reference_columns:
 
-            # Capture subdictionary
             columns = reference_columns[reference]["match_lst"]
-            tracking_number = reference_columns[reference]["Tracking #"]
-            customer = reference_columns[reference]["Customer"]
 
             # Iterate through each pattern-matched column in Extensiv table
             for col in extensiv_table[list[columns]]:
 
                 # Iterate through each value in each column
-                for i, val in enumerate(extensiv_table[col]):
-
-                    # Remove suffix
-                    base_reference = re.sub(r"-s\d+$", "", str(reference))
+                for val in extensiv_table[col]:
 
                     # Create new dictionary entry if match found and populate Dataset class
-                    if val == reference or val == base_reference:
+                    if str(val) == str(reference):
 
-                        match_entry = ReferenceMatch(
-                            Reference=base_reference,
-                            Name=extensiv_table["CustomerIdentifier.Name"][i],
-                            Column=col,
-                            Customer=customer,
-                            Tracking=tracking_number,
-                        )
+                        match_entry = {
+                            "Reference": reference,
+                            "Column": col,
+                            "Customer": self.name,
+                        }
 
                         # Append to list
                         if match_entry not in match_lst:
-                            self.reference_matches.append(match_entry)
+                            self.append_match(
+                                reference_match=f'{match_entry["Reference"]}'
+                            )
                             match_lst.append(match_entry)
 
         return match_lst
 
-    @staticmethod
-    def create_extensiv_receiver_info(extensiv_table: pd.DataFrame) -> dict:
+    def create_extensiv_receiver_info(self, extensiv_table: pd.DataFrame) -> dict:
 
         # Extract receiver information from Extensiv table DataFrame and drop duplicates
         extensiv_receiver_info = extensiv_table.drop_duplicates(
@@ -219,70 +230,68 @@ class PatternMatch:
                 "ShipTo.CompanyName",
                 "ShipTo.Name",
                 "ShipTo.Address1",
-                "CustomerIdentifier.Name",
             ]
         )
-
-        extensiv_receiver_dct = dict()
 
         # Add to dictionary
         for i, row in extensiv_receiver_info.iterrows():
 
-            extensiv_receiver_dct[i] = {
+            self.extensiv_receiver_dct[i] = {
                 "Receiver Address": row["ShipTo.Address1"],
                 "Receiver Company": row["ShipTo.CompanyName"],
                 "Receiver Name": row["ShipTo.Name"],
-                "Customer Identifier": row["CustomerIdentifier.Name"],
             }
 
-        return extensiv_receiver_dct
+    def create_invoice_data_receiver_info(self, invoice_data: pd.DataFrame) -> dict:
 
-    @staticmethod
-    def create_invoice_data_receiver_info(invoice_data: pd.DataFrame) -> dict:  # fmt: skip
+        invoice_data_info = invoice_data.drop_duplicates(
+            ["Receiver Address", "Receiver Company", "Receiver Name"]
+        )
 
-        invoice_data_dct = {}
         # Create new dictionary for invoice_data receiver info
-        for i, row in invoice_data.iterrows():
+        for i, row in invoice_data_info.iterrows():
 
-            invoice_data_dct[i] = {
+            self.invoice_data_receiver_dct[i] = {
                 "Receiver Address": row["Receiver Address"],
                 "Receiver Company": row["Receiver Company"],
                 "Receiver Name": row["Receiver Name"],
-                "Tracking #": row["Tracking #"],
             }
 
-        return invoice_data_dct
-
-    def compare_receiver_info(self, invoice_data_receiver_info: dict, extensiv_receiver_info: dict) -> list[dict]:  # fmt: skip
+    def compare_receiver_info(
+        self, extensiv_table: pd.DataFrame, invoice_data: pd.DataFrame
+    ) -> list[dict]:
 
         match_entry = dict()
         match_lst = list()
-        # Iterate through each dict of invoice_data receiver info
-        for i in invoice_data_receiver_info:
 
+        self.create_extensiv_receiver_info(extensiv_table)
+        self.create_invoice_data_receiver_info(invoice_data)
+
+        # Iterate through each dict of invoice_data receiver info
+        for i in self.invoice_data_receiver_dct:
+            
             # Iterate through each dict of extensiv receiver info
-            for e in extensiv_receiver_info:
+            for e in self.extensiv_receiver_dct:
 
                 # If match, create new dictionary entry
                 if (
-                    invoice_data_receiver_info[i]["Receiver Address"]
-                    == extensiv_receiver_info[e]["Receiver Address"]
-                    or invoice_data_receiver_info[i]["Receiver Name"]
-                    == extensiv_receiver_info[e]["Receiver Name"]
-                    or invoice_data_receiver_info[i]["Receiver Company"]
-                    == extensiv_receiver_info[e]["Receiver Company"]
+                    str(self.invoice_data_receiver_dct[i]["Receiver Address"]).lower().strip() # fmt: skip
+                    == str(self.extensiv_receiver_dct[e]["Receiver Address"]).lower().strip() # fmt: skip
+                    and str(self.invoice_data_receiver_dct[i]["Receiver Name"]).lower().strip()  # fmt: skip
+                    == str(self.extensiv_receiver_dct[e]["Receiver Name"]).lower().strip()  # fmt: skip
+                    and str(self.invoice_data_receiver_dct[i]["Receiver Company"]).lower().strip() # fmt: skip
+                    == str(self.extensiv_receiver_dct[e]["Receiver Company"]).lower().strip() # fmt: skip
                 ):
 
-                    match_entry = ReceiverMatch(
-                        Address=invoice_data_receiver_info[i]["Receiver Address"],
-                        Name=invoice_data_receiver_info[i]["Receiver Name"],
-                        Company=invoice_data_receiver_info[i]["Receiver Company"],
-                        Customer=extensiv_receiver_info[e]["Customer Identifier"],
-                    )
+                    match_entry = {
+                        "Address": self.invoice_data_receiver_dct[i]["Receiver Address"],
+                        "Name": self.invoice_data_receiver_dct[i]["Receiver Name"],
+                        "Company": self.invoice_data_receiver_dct[i]["Receiver Company"],
+                        "Customer": self.name,
+                    }  # fmt: skip
 
-                    #! How is match_entry compared to those already in list?
-                    #! Ex: if there is a matched Name, but not a matched Company, will it be appended?
                     if match_entry not in match_lst:
+                        self.append_match(receiver_match=match_entry)
                         match_lst.append(match_entry)
 
         return match_lst
@@ -294,30 +303,53 @@ class PatternMatch:
         invoice_data_not_qbo: pd.DataFrame,
     ) -> pd.DataFrame:
 
-        try:
+        final_matches_lst = []
+        final_matches_lst.extend(reference_matches)
+        final_matches_lst.extend(receiver_matches)
 
-            # Combine receiver matches and reference matches into one list
-            final_matches_lst = []
-            final_matches_lst.extend(reference_matches)
-            final_matches_lst.extend(receiver_matches)
+        for i, row in invoice_data_not_qbo.iterrows():
 
-            # Iterate through each row in invoice_data DataFrame
-            for i, row in invoice_data_not_qbo.iterrows():
+            for dct in final_matches_lst:
 
-                # Iterate through matches list
-                for dct in final_matches_lst:
+                if "Reference" in dct:
+                    dct["Reference"] = str(dct["Reference"]).strip().lower()
+                if "Address" in dct:
+                    dct["Address"] = str(dct["Address"]).strip().lower()
+                if "Name" in dct:
+                    dct["Name"] = str(dct["Name"]).strip().lower()
+                if "Company" in dct:
+                    dct["Company"] = str(dct["Company"]).strip().lower()
+                
+                #! Might not need this now. This was handled in the above function.
+                # if not pd.isna(row["Reference"]):
+                #     row["Reference"] = str(row["Reference"]).strip().lower()
+                # if not pd.isna(row["Receiver Address"]):
+                #     row["Receiver Address"] = str(row["Receiver Address"]).strip().lower()  # fmt: skip
+                # if not pd.isna(row["Receiver Name"]):
+                #     row["Receiver Name"] = str(row["Receiver Name"]).strip().lower()  # fmt: skip
+                # if not pd.isna(row["Receiver Company"]):
+                #     row["Receiver Company"] = str(row["Receiver Company"]).strip().lower()  # fmt: skip
 
-                    if "Reference" in dct and dct["Reference"] == row["Reference"]:
-                        invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
-                    elif "Address" in dct and dct["Address"] == row["Receiver Address"]:
-                        invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
-                    elif "Name" in dct and dct["Name"] == row["Receiver Name"]:
-                        invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
-                    elif "Company" in dct and dct["Company"] == row["Receiver Company"]:
-                        invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
+                if "Reference" in dct and dct["Reference"] == row["Reference"]:
 
-        #! Or this
-        except TypeError:
-            pass
+                    self.count_reference()
+                    invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
 
-        return invoice_data_not_qbo
+                elif "Address" in dct and dct["Address"] == row["Receiver Address"]:
+
+                    invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
+                    self.count_receiver()
+
+                elif "Name" in dct and dct["Name"] == row["Receiver Name"]:
+
+                    invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
+                    self.count_receiver()
+
+                elif "Company" in dct and dct["Company"] == row["Receiver Company"]:
+
+                    invoice_data_not_qbo.loc[i, "Customer PO #"] = dct["Customer"]
+                    self.count_receiver()
+
+        final_df = invoice_data_not_qbo.drop(columns=["Pattern"])
+
+        return final_df
